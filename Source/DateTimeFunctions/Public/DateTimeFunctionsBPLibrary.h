a7f21f5a9f70ee7bef346f69dab0bc9637545c1e
Mohamad Darwish <122532293+GPUbrainStorm@users.noreply.github.com>
2025-03-15 14:15:15 +0400

    Update DateTimeFunctionsBPLibrary.h

--- Source/DateTimeFunctions/Public/DateTimeFunctionsBPLibrary.h
+++ Source/DateTimeFunctions/Public/DateTimeFunctionsBPLibrary.h
@@ -1,6 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 #pragma once
 
+#include "TimerManager.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "DateTimeFunctionsBPLibrary.generated.h"
 
